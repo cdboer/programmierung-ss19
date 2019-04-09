599e861384a3df73fd087637b23f00c79b630ca5
cdboer <cdboer@cdboer.de>
2019-04-09 11:55:24 +0200

    strip unnecessary whitespace

--- 01-exercise/03-template.hs
+++ 01-exercise/03-template.hs
@@ -2,14 +2,12 @@
 -- Tutor: Claas de Boer
 -- Github: https://github.com/cdboer/programmierung-ss19
 
-
 -- Aufgabe 03A:
 -- Implementieren sie eine Funktion, welche die Zahlen einer Liste aufmultipliziert.
 -- z.B.: prod [1, 2, 3, 4] = 1 * 2 * 3 * 4 = 24
 
 prod :: [Int] -> Int
 -- TODO
-
 
 -- Aufgabe 03B:
 -- Implementieren Sie eine Funktion, welche eine Liste umkehrt.
@@ -18,7 +16,6 @@ prod :: [Int] -> Int
 rev :: [Int] -> [Int]
 -- TODO
 
-
 -- Aufgabe 03C:
 -- Implementieren Sie eine Funktion rem, so dass rem x xs die Liste ist, die aus xs hervorgeht, indem alle Vorkommen von x gelöscht werden.
 -- z.B. rem 2 [1, 2, 3, 2] = [1, 3]
@@ -26,14 +23,12 @@ rev :: [Int] -> [Int]
 rem :: [Int] -> [Int]
 -- TODO
 
-
 -- Aufgabe 03D:
 -- Implementieren Sie eine Funktion, welche für eine Liste prüft, ob sie aufsteigend sortiert ist.
 -- z.B.: isOrd [1, 2, 3] = True oder isOrd [1, 3, 2] = False
 
 isOrd :: [Int] -> Bool
 -- TODO
-
 
 -- Aufgabe 03E:
 -- Implementieren Sie eine Funktion, welche zwei aufsteigend sortierte Listen zu einer aufsteigend sortierten Liste vereinigt.
