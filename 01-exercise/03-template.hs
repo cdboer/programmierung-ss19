3cdda7df47081dafe52f837ad02271b8ee545dfb
cdboer <cdboer@cdboer.de>
2019-04-09 11:42:49 +0200

    add template e01-03

---
+++ 01-exercise/03-template.hs
@@ -0,0 +1,44 @@
+-- Übung 01 - Programmierung TU Dresden
+-- Tutor: Claas de Boer
+-- Github: https://github.com/cdboer/programmierung-ss19
+
+
+-- Aufgabe 03A:
+-- Implementieren sie eine Funktion, welche die Zahlen einer Liste aufmultipliziert.
+-- z.B.: prod [1, 2, 3, 4] = 1 * 2 * 3 * 4 = 24
+
+prod :: [Int] -> Int
+-- TODO
+
+
+-- Aufgabe 03B:
+-- Implementieren Sie eine Funktion, welche eine Liste umkehrt.
+-- z.B.: rev [1, 2, 3] = [3, 2, 1]
+
+rev :: [Int] -> [Int]
+-- TODO
+
+
+-- Aufgabe 03C:
+-- Implementieren Sie eine Funktion rem, so dass rem x xs die Liste ist, die aus xs hervorgeht, indem alle Vorkommen von x gelöscht werden.
+-- z.B. rem 2 [1, 2, 3, 2] = [1, 3]
+
+rem :: [Int] -> [Int]
+-- TODO
+
+
+-- Aufgabe 03D:
+-- Implementieren Sie eine Funktion, welche für eine Liste prüft, ob sie aufsteigend sortiert ist.
+-- z.B.: isOrd [1, 2, 3] = True oder isOrd [1, 3, 2] = False
+
+isOrd :: [Int] -> Bool
+-- TODO
+
+
+-- Aufgabe 03E:
+-- Implementieren Sie eine Funktion, welche zwei aufsteigend sortierte Listen zu einer aufsteigend sortierten Liste vereinigt.
+-- z.B.: merge [1, 3, 5] [2, 4, 6] = [1, 2, 3, 4, 5, 6]
+
+merge [Int] -> [Int] -> [Int]
+-- TODO
+
