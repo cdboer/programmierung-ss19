2b86b1de5ca86ab804adc7de29cd571ca5324756
cdboer <cdboer@cdboer.de>
2019-04-09 11:40:33 +0200

    add template e01-02

---
+++ 01-exercise/02-template.hs
@@ -0,0 +1,10 @@
+-- Übung 02 - Programmierung TU Dresden
+-- Tutor: Claas de Boer
+-- Github: https://github.com/cdboer/programmierung-ss19
+
+-- Aufgabe 02:
+-- Implementieren Sie eine Funktion die bei Eingabe der Zahl i die i-te Zahl der Fibonacci Folge berechnet.
+-- z.B.: fib 3 = 2 oder fib 5 = 8
+
+-- TODO: Implementieren Sie sowohl die Funktion, als auch den Funktionskopf
+
