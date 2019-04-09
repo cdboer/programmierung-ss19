45df83fc3e40c142c61d2cfe091e6fc94245de39
cdboer <cdboer@cdboer.de>
2019-04-09 11:37:38 +0200

    add template e01-01

---
+++ 01-exercise/01-template.hs
@@ -0,0 +1,15 @@
+-- Übung 01 - Programmierung TU Dresden
+-- Tutor: Claas de Boer
+-- Github: https://github.com/cdboer/programmierung-ss19
+
+-- Aufgabe 01A:
+-- Implementieren Sie eine Funktion fac, so dass fac n die Fakultät von n berechnet.
+-- z.B.: fac 3 = 6 oder fac 4 = 24 oder fac 13 = 6227020800
+fac :: Int -> Int
+-- TODO
+
+-- Aufgabe 01B:
+-- Implementieren Sie eine Funktion sumFacs, so dass sumFacs n m die Summe der Fakultäten der natürlichen Zahlen von n bis m berechnet.
+-- z.B.: sumFacs 2 3 = 2! + 3! = 2 + 6 = 8
+sumFacs :: Int -> Int -> Int
+-- TODO
