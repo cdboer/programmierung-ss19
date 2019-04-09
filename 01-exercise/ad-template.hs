38da3fe571026dff9ad50ac8790c9ff7520be35e
cdboer <cdboer@cdboer.de>
2019-04-09 11:54:44 +0200

    add template e01-ad

---
+++ 01-exercise/ad-template.hs
@@ -0,0 +1,15 @@
+-- Übung 01 - Programmierung TU Dresden
+-- Tutor: Claas de Boer
+-- Github: https://github.com/cdboer/programmierung-ss19 
+
+-- Aufabe ad01:
+-- Implementieren Sie die (unendliche) Liste der Fibonacci-Zahlen f0, f1, ...
+
+fibs :: [Int]
+-- TODO
+
+-- Aufgabe ad02:
+-- In einem vollen Binärbaum ist jeder Knoten entweder ein Blatt, oder er hat zwei Kinderknoten.
+-- Implementieren Sie eine Funktion, welche für n ∈ ℕ die Anzahl der vollen Binärbäume mit Knotenzahl n berechnet.
+
+-- TODO
